f27d7767e51177bf4f16afebb242a8e3835c9300
kalistareddy1 <ST10459332@vcconnect.edu.za>
2025-09-28 20:30:17 +0200

    Initializing Git Repository

---
+++ PracTest_Question4_1/src/main/java/com/mycompany/practest_question4_1/PracTest_Question4_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practest_question4_1;
+
+/**
+ *
+ * @author kalis
+ */
+public class PracTest_Question4_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ PracTest_Question4_2/src/main/java/com/mycompany/practest_question4_2/PracTest_Question4_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practest_question4_2;
+
+/**
+ *
+ * @author kalis
+ */
+public class PracTest_Question4_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
